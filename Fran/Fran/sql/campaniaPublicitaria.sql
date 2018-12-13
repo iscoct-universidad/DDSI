0e168716b89dd389abce5b9e8cc0954e808ef6d5
iscoct <iscoct@correo.ugr.es>
2018-12-14 00:17:28 +0100

    Delete campaniaPublicitaria.sql

--- Fran/Fran/sql/campaniaPublicitaria.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE CampaniaPublicitaria (
-  CodEnt          INT   NOT NULL  REFERENCES Entidad(CodEnt),
-  Tipo            VARCHAR(50)  NOT NULL,
-  PublicoObjetivo VARCHAR(50)  NOT NULL
-);
