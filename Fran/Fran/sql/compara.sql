d0e0a488b5d6223fe906dfeb346e2b84f0b434d0
iscoct <iscoct@correo.ugr.es>
2018-12-14 00:17:34 +0100

    Delete compara.sql

--- Fran/Fran/sql/compara.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Compara (
-  CodProdComp   VARCHAR(9)    NOT NULL  REFERENCES ProductoCompetidor(CodProdComp),
-  CodProd       VARCHAR(9)    NOT NULL  REFERENCES Producto(CodProd),  
-  CodComp       VARCHAR(10)   NOT NULL,
-  Informe       VARCHAR(100),
-  PRIMARY KEY (CodProdComp, CodProd, CodComp)
-);
