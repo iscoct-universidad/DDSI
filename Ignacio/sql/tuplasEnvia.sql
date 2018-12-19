ac89d1874a3b90f549294b70ea600aa1b66cb56a
Igunashio-sama <ignaciobarloz@correo.ugr.es>
2018-12-19 17:41:54 +0100

    Inserción de tuplas separada

---
+++ Ignacio/sql/tuplasEnvia.sql
@@ -0,0 +1,5 @@
+INSERT INTO Envia VALUES (0, '1', 6, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
+INSERT INTO Envia VALUES (1, '2', 8, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
+INSERT INTO Envia VALUES (2, '6', 3, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
+INSERT INTO Envia VALUES (3, '1', 5, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
+INSERT INTO Envia VALUES (4, '6', 9, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
