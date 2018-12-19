a957a0e18b5d540e4384624ea81a5e6a3afff29f
Igunashio-sama <ignaciobarloz@correo.ugr.es>
2018-12-19 17:47:48 +0100

    Claves externas corregidas

--- Ignacio/sql/distribuidor.sql
+++ Ignacio/sql/distribuidor.sql
@@ -1,4 +1,10 @@
 CREATE TABLE Distribuidor (
-  CodEnt       INT    PRIMARY KEY,
+  CodEnt       INT,
   Nombre VARCHAR(20)
+
+  PRIMARY KEY (CodEnt),
+  CONSTRAINT CodEnt_fk
+    FOREIGN KEY (CodEnt)
+    REFERENCES Entidad(CodEnt)
+    ON DELETE CASCADE
 );
--- Ignacio/sql/envia.sql
+++ Ignacio/sql/envia.sql
@@ -1,7 +1,16 @@
 CREATE TABLE Envia (
-  CodProd INT    NOT NULL  REFERENCES Producto(CodProd),
-  CodEnt INT    NOT NULL  REFERENCES Distribuidor(CodEnt),  
+  CodProd INT    NOT NULL,
+  CodEnt INT    NOT NULL,  
   Fecha  VARCHAR(10)   NOT NULL,
   Cantidad  INT   NOT NULL,
-  PRIMARY KEY (CodProd, CodEnt, Fecha)
+
+  PRIMARY KEY (CodProd, CodEnt, Fecha),
+  CONSTRAINT CodProd_fk
+    FOREIGN KEY (CodProd)
+    REFERENCES Producto(CodProd)
+    ON DELETE CASCADE,
+  CONSTRAINT CodEnt_Envia_fk
+    FOREIGN KEY (CodEnt)
+    REFERENCES Distribuidor(CodEnt)
+    ON DELETE CASCADE
 );
