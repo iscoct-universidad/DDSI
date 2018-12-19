027c61d2bd52ee54914e3c2a4988bf725cdefe07
Igunashio-sama <ignaciobarloz@correo.ugr.es>
2018-12-19 19:15:00 +0100

    Corregidos errores

--- Ignacio/sql/distribuidor.sql
+++ Ignacio/sql/distribuidor.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Distribuidor (
-  CodEnt       INT,
-  Nombre VARCHAR(20)
+  CodEnt       INT  NOT NULL,
+  Nombre VARCHAR(20),
 
   PRIMARY KEY (CodEnt),
   CONSTRAINT CodEnt_fk
--- Ignacio/sql/tuplasEnvia.sql
+++ Ignacio/sql/tuplasEnvia.sql
@@ -1,5 +1,5 @@
-INSERT INTO Envia VALUES (0, '1', 6, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
-INSERT INTO Envia VALUES (1, '2', 8, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
-INSERT INTO Envia VALUES (2, '6', 3, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
-INSERT INTO Envia VALUES (3, '1', 5, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
-INSERT INTO Envia VALUES (4, '6', 9, TO_CHAR(SYSDATE, 'dd-mm-yyyy'));
+INSERT INTO Envia VALUES (0, '1', '10-11-2018', 6);
+INSERT INTO Envia VALUES (1, '2', '10-11-2018', 8);
+INSERT INTO Envia VALUES (2, '6', '10-11-2018', 3);
+INSERT INTO Envia VALUES (3, '1', '10-11-2018', 5);
+INSERT INTO Envia VALUES (4, '6', '10-11-2018', 9);
