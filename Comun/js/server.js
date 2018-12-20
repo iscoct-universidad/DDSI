2905e7f18724c79c50bc38c4714e52c7df58ce5f
iscoct7@gmail.com <iscoct@correo.ugr.es>
2018-12-20 12:10:55 +0100

    Primera aproximación a la visualización de la BD entera

--- Comun/js/server.js
+++ Comun/js/server.js
@@ -1,6 +1,27 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const operacionesComunes = require('./operaciones');
+
+const construirArrayBD = async (result) => {
+	return new Promise((resolve, reject) => {
+		let solucion = [];
+		let sql;
+		
+		result.forEach((value, index, array) => {
+			sql = "select * from " + value;
+			
+			con.query(sql, (err, tuplas) => {
+				solucion.push(sql);
+			});
+		});
+	
+		console.log("Solución: ", solucion);
+		
+		resolve(solucion);
+		reject("");
+	});
+}
 
 var server = http.createServer((req, res) => {
 	var path = url.parse(req.url).pathname;
@@ -9,11 +30,36 @@ var server = http.createServer((req, res) => {
 	while(path.indexOf('/') == 0)
 		path = path.slice(1);
 
-	if (path == "/" || path == "")
+	if(path == "/" || path == "")
 		caminoALeer = "index.html";
-	else if ("favicon.ico" == path)
+	else if("favicon.ico" == path)
 		caminoALeer = "error";
-	else
+	else if(path == "mostrarBD.html") {
+		let sql1 = "show tables;"
+		let sql;
+		let respuesta = "";
+		
+		operacionesComunes.conectarse((err, con) => {
+			if(err)
+				console.log("Hubo un error al conectarse a la BD en el server común");
+			else {
+				con.query(sql1, (err, result) => {
+					let promesa = construirArrayBD;
+					
+					promesa.then((array) => {
+						respuesta += "<ul>";
+						
+						for(let x of array)
+							respuesta += "<li>" + x + "</li>"
+						
+						respuesta += "</ul>";
+						
+						operacionesComunes.devolverRespuesta(respuesta);
+					});
+				});
+			}
+		});
+	} else
 		caminoALeer = path;
 
 	console.log("Camino a leer: ", caminoALeer);
