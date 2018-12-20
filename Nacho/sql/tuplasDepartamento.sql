9a8b9341696acda00a207526a759df3a8ea7028a
Juan Manuel Rubio Rodriguez <juanmrr89@gmail.com>
2018-12-20 17:52:30 +0100

    BD actualizada

--- Nacho/sql/tuplasDepartamento.sql
+++ Nacho/sql/tuplasDepartamento.sql
@@ -1,3 +1,3 @@
-INSERT INTO Departamentos VALUES ('0','A', 'Spain'), ('1','B', 'Spain')
-,('2','C', 'Spain'), ('3','D', 'Spain'), ('4','E', 'Spain'), ('5','F', 'Spain')
-,('6','G', 'Spain'), ('7','H', 'Spain'), ('8','I', 'Spain'), ('9','J', 'Spain');
+INSERT INTO Departamentos VALUES ('0','Granada', 'Spain'), ('1','Almeria', 'Spain')
+,('2','Bilbao', 'Spain'), ('3','Barcelona', 'Spain'), ('4','Madrid', 'Spain'), ('5','Sevilla', 'Spain')
+,('6','Valladolid', 'Spain'), ('7','Malaga', 'Spain'), ('8','Lugo', 'Spain'), ('9','Valencia', 'Spain');
