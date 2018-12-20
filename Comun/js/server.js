abda9d70af20cfa279696d57eb0cf5498e83cd28
iscoct7@gmail.com <iscoct@correo.ugr.es>
2018-12-20 14:11:58 +0100

    Mostrado de todas las tablas de la base de datos en server.js

--- Comun/js/server.js
+++ Comun/js/server.js
@@ -3,23 +3,41 @@ const http = require('http');
 const url = require('url');
 const operacionesComunes = require('./operaciones');
 
-const construirArrayBD = async (result) => {
+const contenidoTabla = (con, nombreTabla) => {
 	return new Promise((resolve, reject) => {
-		let solucion = [];
-		let sql;
+		let respuesta = "";
+		let sql = "select * from " + nombreTabla + ";";
 		
-		result.forEach((value, index, array) => {
-			sql = "select * from " + value;
-			
-			con.query(sql, (err, tuplas) => {
-				solucion.push(sql);
-			});
+		con.query(sql, (err, result) => {
+			if(err) {
+				respuesta = "Hubo un error en la consulta a la tabla " + nombreTabla;
+				console.log(respuesta);
+				console.log("Error: ", err);
+				
+				reject(respuesta);
+			} else {
+				respuesta += "<h2>" + nombreTabla + "</h2>";
+				respuesta += "<table><tr>";
+				
+				for(let x in result[0])
+					respuesta += "<th>" + x + "</th>";
+				
+				respuesta += "</tr>";
+				
+				for(let y of result) {
+					respuesta += "<tr>";
+					
+					for(let z in y)
+						respuesta += "<th>" + y[z] + "</th>";
+					
+					respuesta += "</tr>";
+				}
+				
+				respuesta += "</table>";
+				
+				resolve(respuesta);
+			}
 		});
-	
-		console.log("Solución: ", solucion);
-		
-		resolve(solucion);
-		reject("");
 	});
 }
 
@@ -35,36 +53,39 @@ var server = http.createServer((req, res) => {
 	else if("favicon.ico" == path)
 		caminoALeer = "error";
 	else if(path == "mostrarBD.html") {
-		let sql1 = "show tables;"
-		let sql;
-		let respuesta = "";
-		
+		let respuesta = [];
+	
 		operacionesComunes.conectarse((err, con) => {
 			if(err)
-				console.log("Hubo un error al conectarse a la BD en el server común");
-			else {
-				con.query(sql1, (err, result) => {
-					let promesa = construirArrayBD;
+				console.log("Hubo un error al conectarse a la BD");
+			
+			let nombresTablas = [];
+			let sql = "show tables;";
+			
+			con.query(sql, (err, result) => {
+				for(let x of result)
+					for(let y in x)
+						nombresTablas.push(x[y]);
+				
+				let promesas = [];
+				
+				for(let z of nombresTablas)
+					promesas.push(contenidoTabla(con, z));
 					
-					promesa.then((array) => {
-						respuesta += "<ul>";
-						
-						for(let x of array)
-							respuesta += "<li>" + x + "</li>"
-						
-						respuesta += "</ul>";
-						
-						operacionesComunes.devolverRespuesta(respuesta);
-					});
+				Promise.all(promesas).then((values) => {
+					for(let x of values)
+						respuesta += x;
+					
+					operacionesComunes.devolverRespuesta(res, respuesta);
 				});
-			}
+			});
 		});
 	} else
 		caminoALeer = path;
 
 	console.log("Camino a leer: ", caminoALeer);
 
-	if (caminoALeer != "error") {
+	if (caminoALeer != "error" && caminoALeer != undefined) {
 		fs.readFile(caminoALeer, "utf-8", (err, data) => {
 			if(err)
 				console.log("Hubo un error al leer el fichero");
